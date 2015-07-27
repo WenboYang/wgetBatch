35f3db6215832b1d147502b61245da4d487612b3
WenboYang <wenbo.yangcn@gmail.com>
2015-07-28 00:08:33 +0800

    rename .html with current timing

--- weiboPublic/getyz.bat
+++ weiboPublic/getyz.bat
@@ -1,3 +1,6 @@
-call wget %1 -O yz.gz
+call rm yz
+call wget weibo.cn/1841158660 -O yz.gz
 call gzip -d yz.gz 
-call ren yz yz.html
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-4 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b%%c%%d)
+call ren yz yz%mydate%_%mytime%_%RANDOM%.html
